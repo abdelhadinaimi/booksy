44a1aff1b037d301494777c11179d27c8e6e58f8
yassineCherifi <mohammedyassine.cherifi@student.univ-tlemcen.dz>
2019-11-22 10:55:31 +0100

    add auth0

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,18 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BooksComponent } from './components/books/books.component';
 import { ShelvesComponent } from './components/shelves/shelves.component';
+import { AuthGuard } from './services/auth/auth.guard';
+import { CallbackComponent } from './components/callback/callback.component';
 
 
 const routes: Routes = [
   {path: '',component: HomeComponent, 
   children: [
   {path: 'books', component:BooksComponent},
-  {path: 'shelves', component:ShelvesComponent}
+  {path: 'shelves', component:ShelvesComponent},
+  {path: 'callback', component:CallbackComponent}
   ]}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ client/src/app/services/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.loggedIn) {
+      this.router.navigate(['/books']);
+      return false;
+    }
+    return true;
+  }
+}
