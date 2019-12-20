6dc1b5edd1d939ea84bfbab0a724c9edc3e4e0e9
yassineCherifi <mohammedyassine.cherifi@student.univ-tlemcen.dz>
2019-12-20 20:05:41 +0100

    search component

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../../services/book.service/book.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  books = [];
+  constructor(private route: ActivatedRoute, private bookService: BookService) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.searchBooks(params['q']);
+    });
+
+  }
+
+  searchBooks(q) {
+    this.bookService.getBooks(q).subscribe(data => {
+      this.books = data['items'];
+      console.log(data['items']);
+    });
   }
 
 }
