5ff085053a1f41192c4e611dcabf3f5bdcef3163
yassineCherifi <mohammedyassine.cherifi@student.univ-tlemcen.dz>
2019-12-23 23:53:00 +0100

    component search design

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,10 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   title = 'Booksy';
-  contentSearch;
-  constructor(public auth: AuthService){}
+  constructor(public auth: AuthService,private router : Router){}
+
+  searchBooks(form : NgForm){
+    this.router.navigateByUrl('/search?q='+form.value.contentSearch);
+  }
 }
 
